cdea1fa1f8be18da93b55ac48b881ecf15ac5dc6
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-05-09 16:01:41 +0800

    added axios

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
-import { useReducer } from 'react';
+import axios from 'axios';
+import { useState, useEffect, useReducer } from 'react';
 import {
   BrowserRouter,
   Route,
@@ -11,9 +12,21 @@ import { MovieMenu } from '../MovieMenu';
 
 import { AppWrapper } from './App.styled';
 import { MovieDetails } from '../MovieDetails';
+import { Movie } from '../../models';
+
+const client = axios.create({
+  baseURL: "https://127.0.0.1:8003/movies" 
+});
 
 function App() {
-  const [movieList, dispatch] = useReducer(movieReducer, initialMovieList);
+  // const [movieList, dispatch] = useReducer(movieReducer, initialMovieList);
+  const [movieList, setMovieList] = useState(null);
+
+  useEffect(() => {
+    client.get().then((response: Movie) => {
+      setMovieList(response);
+    });
+  }, []);
 
   return (
     <MovieContext.Provider value={movieList}>
